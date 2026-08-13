3e99a4f3ef0735225f1c59251d909103e8df934c
masssal0501 <masssal0501@gmail.com>
2026-08-13 17:00:25 +0900

    Spring Backend Install

---
+++ WorkFlow_Project_BE/src/main/java/com/kh/workflow/WorkFlowProjectBeApplication.java
@@ -0,0 +1,13 @@
+package com.kh.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFlowProjectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkFlowProjectBeApplication.class, args);
+	}
+
+}
