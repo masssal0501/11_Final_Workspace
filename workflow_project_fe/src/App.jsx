e73f8e153621c8ca200c5e087998895e215ccc64
masssal0501 <masssal0501@gmail.com>
2026-08-19 16:40:47 +0900

    로그인 도메인 작업
    LoginForm, FindIDForm, FindPWForm 생성
    EmployeeEnrollForm 작업중

--- workflow_project_fe/src/App.jsx
+++ workflow_project_fe/src/App.jsx
@@ -3,46 +3,41 @@ import './App.css'
 
 import Header from "./common/components/Header";
 import Footer from "./common/components/Footer";
-import LoginForm from './common/components/LoginForm';
-import FindIDForm from './common/components/FindIDForm';
-import FindPWForm from './common/components/FindPWForm';
+import LoginForm from './login/component/LoginForm';
+import FindIDForm from './login/component/FindIDForm';
+import FindPWForm from './login/component/FindPWForm';
 
 import { Routes, Route, Navigate } from "react-router-dom";
+
+import EmployeeEnrollFormComponent from './employee/components/EmployeeEnrollFormComponent';
 
 function App() {
 
   const [loginUser, setLoginUser] = useState(sessionStorage.getItem("loginUser"));
 
-  if(loginUser == null) {
+  if(loginUser != null) { // 권한으로 분기점 샏성해야함 (추후 "==" 으로 수정)
   
     return(
-      <div>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
 
-        <Header />
-
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Navigate to="/login" replace />} />
-
-            <Route path="/login" element={<LoginForm />}></Route>
-            <Route path="/login/findID" element={<FindIDForm />}></Route>
-            <Route path="/login/findPW" element={<FindPWForm />}></Route>
-          </Routes>
-        </div>
-
-        <Footer />
+          <Route path="/login" element={<LoginForm />}></Route>
+          <Route path="/login/findID" element={<FindIDForm />}></Route>
+          <Route path="/login/findPW" element={<FindPWForm />}></Route>
+        </Routes>
       </div>
     );
-  } else {
+  } else { // 관리자일 경우
 
     return(
 
       <div>
         <Header />
 
-          <div className="content">
-            로그인 성공
-          </div>
+        <Routes>
+          <Route path="/employee/enrollForm" element={<EmployeeEnrollFormComponent />}></Route>
+        </Routes>
 
         <Footer />
       </div>
