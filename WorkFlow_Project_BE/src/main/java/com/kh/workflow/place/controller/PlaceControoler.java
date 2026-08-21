727ce204d536fdc67ca2cae4b8937c079d50ef71
Gyumin7870 <kgmlove230@gmail.com>
2026-08-21 12:50:36 +0900

    지역 정보 백앤드 작업
    컨트롤러 서비스 서비스 임플 초기 작업

--- WorkFlow_Project_BE/src/main/java/com/kh/workflow/place/controller/PlaceControoler.java
+++ WorkFlow_Project_BE/src/main/java/com/kh/workflow/place/controller/PlaceControoler.java
@@ -1,5 +1,23 @@
 package com.kh.workflow.place.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.workflow.place.model.service.PlaceService;
+
+@CrossOrigin
+@RestController
 public class PlaceControoler {
+	
+	private String secretKey;
+	
+	private PlaceService placeService;
+	
+	public void selectPlaceList() {
+		
+		
+	}
+	
+	
 
 }
--- WorkFlow_Project_BE/src/main/java/com/kh/workflow/place/model/service/PlaceServiceImpl.java
+++ WorkFlow_Project_BE/src/main/java/com/kh/workflow/place/model/service/PlaceServiceImpl.java
@@ -1,5 +1,34 @@
 package com.kh.workflow.place.model.service;
 
-public class PlaceServiceImpl {
+import java.util.List;
+
+import com.kh.workflow.place.model.vo.Place;
+
+public class PlaceServiceImpl implements PlaceService {
+
+	@Override
+	public List<Place> selectPlaceList() {
+		return null;
+	}
+
+	@Override
+	public Place selectPlace(int hubNo) {
+		return null;
+	}
+
+	@Override
+	public Place insertPlace(Place p) {
+		return null;
+	}
+
+	@Override
+	public Place updatePlace(Place p) {
+		return null;
+	}
+
+	@Override
+	public int deletePlace(int hubNo) {
+		return 0;
+	}
 
 }
