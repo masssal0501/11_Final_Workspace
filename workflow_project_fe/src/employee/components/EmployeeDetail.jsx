e7f026e9c8e673eaa6e73e2ebbbc3ab07f950c3c
masssal0501 <masssal0501@gmail.com>
2026-08-27 17:46:26 +0900

    0827 작업
    직원정보수정 페이지 작업중 (EmployeeEdit.jsx)

--- workflow_project_fe/src/employee/components/EmployeeDetail.jsx
+++ workflow_project_fe/src/employee/components/EmployeeDetail.jsx
@@ -17,6 +17,94 @@ function EmployeeDetail() {
 
     }, [empNo]);
 
+    // 직책
+    const getAuthName = (authCode) => {
+
+        switch (authCode) {
+
+            case "ADMIN":
+                return "관리자";
+
+            case "MANAGER":
+                return "부서장";
+
+            case "STAFF":
+                return "평사원";
+
+            default:
+                return "";
+        }
+    };
+
+    // 부서
+    const getDepName = (depId) => {
+
+        switch (depId) {
+
+            case "D1":
+                return "기획";
+
+            case "D2":
+                return "디자인";
+
+            case "D3":
+                return "FE 개발";
+            
+            case "D4":
+                return "BE 개발";
+            
+            case "D5":
+                return "데이터";
+
+            case "D6":
+                return "QA";
+
+            default:
+                return "";
+        }
+    };
+
+
+    // 직위
+    const getJobName = (jobCode) => {
+
+        switch (jobCode) {
+
+            case "J1":
+                return "사원";
+
+            case "J2":
+                return "대리";
+
+            case "J3":
+                return "과장";
+
+            case "J4":
+                return "차장";
+
+            case "J5":
+                return "부장";
+
+            default:
+                return "";
+        }
+    };
+
+    // 계정 상태
+    const getStatusName = (status) => {
+
+        switch (status) {
+
+            case "Y":
+                return "활성";
+
+            case "N":
+                return "비활성";
+
+            default:
+                return "";
+        }
+    };
 
     const loadEmployee = async () => {
 
@@ -73,7 +161,7 @@ function EmployeeDetail() {
                             </td>
                             <th>직책</th>
                             <td>
-                                {employee.authCode}
+                                {getAuthName(employee.authCode)}
                             </td>
                             
                         </tr>
@@ -84,7 +172,7 @@ function EmployeeDetail() {
                             </td>
                             <th>부서</th>
                             <td>
-                                {employee.depId}
+                                {getDepName(employee.depId)}
                             </td>
                             
                         </tr>
@@ -95,7 +183,7 @@ function EmployeeDetail() {
                             </td>
                             <th>직위</th>
                             <td>
-                                {employee.jobCode}
+                                {getJobName(employee.jobCode)}
                             </td>
                         </tr>
                         <tr>
@@ -107,7 +195,7 @@ function EmployeeDetail() {
                         <tr>
                             <th>상태</th>
                             <td>
-                                {employee.status}
+                                {getStatusName(employee.status)}
                             </td>
                         </tr>
                     </tbody>
--- workflow_project_fe/src/employee/components/EmployeeEnrollFormComponent.jsx
+++ workflow_project_fe/src/employee/components/EmployeeEnrollFormComponent.jsx
@@ -1,8 +1,12 @@
 import "../styles/EmployeeEnrollFormComponent.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { createEmployee, checkEmpIdDuplicate } from "../api/employeeApi";
 
 function EmployeeEnrollFormComponent() {
+
+    const navigate = useNavigate();
+
     // 숫자만 입력되도록 제한하는 함수
     const handlePhoneChange = (e) => {
 
@@ -260,7 +264,8 @@ function EmployeeEnrollFormComponent() {
                         </tr>
                     </tbody>
                 </table>
-                <button type="button">돌아가기</button>
+                <button type="button"
+                onClick={() => navigate(-1)}>돌아가기</button>
                 <button type="submit">직원 등록</button>
 
             </form>
