383bc2d1a1da13e280429ec073e8a1d01a6654c9
min-dro <serominsockji9n9@gmail.com>
2026-09-07 09:13:36 +0900

    Dto 주석

--- WorkFlow_Project_BE/src/main/java/com/kh/workflow/dashboard/model/dto/ChartDataDto.java
+++ WorkFlow_Project_BE/src/main/java/com/kh/workflow/dashboard/model/dto/ChartDataDto.java
@@ -6,6 +6,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * 차트 시각화 데이터 표현을 위한 공통 DTO
+ * 대시보드 화면의 파이 차트, 바 차트, 라인 차트 등에서 
+ * 항목 이름(라벨)과 수치(값) 데이터를 묶어서 전달하기 위해 사용됩니다.
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
