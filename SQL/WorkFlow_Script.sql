6bcac0bdec62c0ae5922a82f9f0108113ce39c3d
masssal0501 <masssal0501@gmail.com>
2026-09-09 00:05:33 +0900

    Table script 최신화

--- SQL/WorkFlow_Script.sql
+++ SQL/WorkFlow_Script.sql
@@ -27,6 +27,7 @@ DROP TABLE IF EXISTS notice_file;
 DROP TABLE IF EXISTS survey_answer;
 DROP TABLE IF EXISTS workcation_survey;
 DROP TABLE IF EXISTS survey_question;
+DROP TABLE IF EXISTS task_history;
 DROP TABLE IF EXISTS work_file;
 DROP TABLE IF EXISTS task;
 DROP TABLE IF EXISTS work;
@@ -157,7 +158,7 @@ CREATE TABLE hub (
         COMMENT '거점 상세정보',
 
     hub_type INT NULL
-        COMMENT '1 공유오피스, 2 숙소, 3 제휴시설',
+        COMMENT '1 공유오피스, 2 숙소, 3 체험프로그램, 4 맛집, 5 관광지',
         
 	max_capacity INT NULL
         COMMENT '최대 수용인원',
@@ -321,6 +322,9 @@ CREATE TABLE task (
 
     taskend_at TIMESTAMP NULL
         COMMENT '작업 종료일시',
+        
+	progress INT NOT NULL DEFAULT 0
+    COMMENT '작업 진행률 0~100',
 
     status VARCHAR(1) NOT NULL DEFAULT 'N'
         COMMENT 'N 미완료, Y 완료',
@@ -336,6 +340,31 @@ CREATE TABLE task (
         REFERENCES work (work_no)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+/* 작업 기록*/
+CREATE TABLE task_history (
+    history_no INT NOT NULL AUTO_INCREMENT
+        COMMENT 'PK',
+
+    history_title VARCHAR(200) NOT NULL,
+
+    history_content VARCHAR(1000) NOT NULL,
+
+    progress INT NOT NULL DEFAULT 0
+        COMMENT '작업 진행률',
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        COMMENT '업무 진행 기록 시간',
+
+    task_no INT NOT NULL
+        COMMENT '작업 PK',
+
+    CONSTRAINT pk_task_history
+        PRIMARY KEY (history_no),
+
+    CONSTRAINT fk_task_history_task
+        FOREIGN KEY (task_no)
+        REFERENCES task (task_no)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; 
 
 /* 작업 첨부파일 */
 CREATE TABLE work_file (
@@ -410,14 +439,17 @@ CREATE TABLE amount (
 CREATE TABLE amount_item (
     item_no INT NOT NULL AUTO_INCREMENT,
 
-    amountamountitem_type VARCHAR(15) NOT NULL
+    item_type VARCHAR(15) NOT NULL
         COMMENT 'S 숙박, T 교통, E 체험, F 식비, V 차량, O 기타',
-
-    item_date TIMESTAMP NOT NULL
-        COMMENT '비용 발생일시',
 
     item_approved VARCHAR(15) NULL
         COMMENT 'A 승인, C 취소, H 보류, J 반려, R 검토',
+        
+	item_approved_amount INT NOT NULL DEFAULT 0
+		COMMENT '승인 비용',
+    
+	item_date TIMESTAMP NOT NULL
+        COMMENT '비용 발생일시',
 
     item_description VARCHAR(500) NULL
         COMMENT '반려된 비용 참고 설명',
@@ -701,6 +733,9 @@ CREATE INDEX idx_work_workcation
 
 CREATE INDEX idx_task_work
     ON task (work_no);
+    
+CREATE INDEX idx_task_history_task
+    ON task_history (task_no);
 
 
 /* 비용 조회 */
