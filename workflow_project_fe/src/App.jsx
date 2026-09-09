d3402f6f4832a6c52e535ca7e49ac046143c8f24
Gyumin7870 <kgmlove230@gmail.com>
2026-09-09 16:48:41 +0900

    라우트 경로 변경
    지역정보 라우트 변경

--- workflow_project_fe/src/App.jsx
+++ workflow_project_fe/src/App.jsx
@@ -260,10 +260,10 @@ function App() {
                 <Route path="/hub/ai" element={ <AIComponent/> }></Route>
 
                 {/* place */}
-                <Route path="/workflow/place/list" element={ <PlaceList /> } />
-                <Route path="/workflow/place/Form" element={ <PlaceForm /> } />
-                <Route path="/workflow/place/detail/:hubNo" element={ <PlaceDetail /> } />
-                <Route path="/workflow/place/edit/:hubNo" element={ <PlaceEdit /> } />
+                <Route path="/place/list" element={ <PlaceList /> } />
+                <Route path="/place/Form" element={ <PlaceForm /> } />
+                <Route path="/place/detail/:hubNo" element={ <PlaceDetail /> } />
+                <Route path="/place/edit/:hubNo" element={ <PlaceEdit /> } />
                 
                 {/* 업무 게시판 라우트 */}
                 <Route path="/task/list" element={<TaskListComponent />} />
